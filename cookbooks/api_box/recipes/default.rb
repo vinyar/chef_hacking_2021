b55577b48afd8ffa0fe87999475b26e86b53d10b
Alex Vinyar <avinyar@launchcg.com>
2021-03-15 15:56:13 -0700

    adding a pile of resources to demo cookbook to showcase community cookbook; \n\n removing policyfile because i forgot how to use them, and adding berks instead to help with local cookbook resolution

--- cookbooks/api_box/recipes/default.rb
+++ cookbooks/api_box/recipes/default.rb
@@ -4,9 +4,67 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-directory 'c:\alex\was\here' do
+directory 'making a mark' do
+  path 'c:\alex\was\here'
   recursive true
   action :create
 end
 
 directory 'c:\bob'
+
+windows_feature_powershell 'installing file services' do
+  feature_name 'RSAT-File-Services'
+  all false
+  # source 'localrepository'
+  action :install
+end
+
+
+# Using community IIS cookbook to install and configure IIS
+
+# This really lives in attributes, but showing here for simplicity.
+node.default['api_box']['windows_features']       = %w(Web-Http-Errors IIS-NetFxExtensibility IIS-ASPNET)
+
+include_recipe 'iis'
+
+# adding .net 45 support via community IIS cookbook
+include_recipe 'iis::mod_ftp'
+
+# adding management tools via community IIS cookbook
+include_recipe 'iis::mod_management'
+
+# remove default site
+include_recipe 'iis::remove_default_site'
+
+# using community IIS cookbook to make some new sites
+iis_site 'alex-was-here' do
+  port 4321
+  path 'c:\alex\was\here'
+  protocol :ftp
+  host_header 'i_like_cupcakes'
+  running true
+end
+
+
+iis_section 'unlocks windows authentication control in web.config' do
+  site 'alex-was-here'
+  section 'system.webServer/security/authentication/windowsAuthentication'
+  action :unlock
+end
+
+node.default['api_box']['dev'] = 'true'
+
+iis_section 'unlocks anonymous authentication control in web.config' do
+  site 'alex-was-here'
+  section 'system.webServer/security/authentication/anonymousAuthentication'
+  action :unlock
+  only_if { node['api_box']['dev'] }
+end
+
+
+reboot 'reboot if pending' do
+  reason 'Need to reboot when the run completes successfully.'
+  delay_mins 0
+  action :request_reboot
+  only_if {reboot_pending?}
+end
--- cookbooks/api_box/spec/unit/recipes/default_spec.rb
+++ cookbooks/api_box/spec/unit/recipes/default_spec.rb
@@ -10,6 +10,16 @@ describe 'api_box::default' do
   context 'When all attributes are default, on Ubuntu 20.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'windows'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+
+  context 'When all attributes are default, on Ubuntu 20.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
     platform 'ubuntu', '20.04'
 
     it 'converges successfully' do
