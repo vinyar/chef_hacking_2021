a5a9e4df4c2661ced91506e88320111a3f491d14
Alex Vinyar <avinyar@launchcg.com>
2021-03-15 20:12:33 -0700

    adding Berksfile and a few tests to inspec

--- cookbooks/api_box/test/integration/default/default_spec.rb
+++ cookbooks/api_box/test/integration/default/default_spec.rb
@@ -8,17 +8,13 @@ describe directory('c:\bob') do
   it { should exist }
 end
 
-# describe service('W3SVC') do
-#   it { should be_installed }
-#   it { should be_running }
-#   its('startmode') { should eq 'Auto' }
-# end
-#
-# # Unless we are on a 'polluted' machine, the default website should
-# # be present if the IIS Role was freshly installed. All our vagrant
-# # configurations install with the system drive at C:\
-# describe iis_site('Default Web Site') do
-#   it { should exist }
-#   it { should be_running }
-#   it { should have_app_pool('DefaultAppPool') }
-# end
+describe service('W3SVC') do
+  it { should be_installed }
+  it { should be_running }
+  its('startmode') { should eq 'Auto' }
+end
+
+describe iis_site('Default Web Site') do
+  it { should not_exist }
+  it { should have_app_pool('DefaultAppPool') }
+end
